39df9d47862fdc395a7bf73f891cb23b269b4381
pawan-meena <pawanmeenazee@outlook.com>
2022-05-26 18:42:32 +0530

    check

--- stream.js
+++ stream.js
@@ -22,7 +22,7 @@ var isMobile = {
         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 };
-var embedded = "mobile";
+var embedded = "desktop";
 if (!isMobile.any()){
         embedded = "desktop";
 }
